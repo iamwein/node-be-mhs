13208d1d561b68c88183c225b4d420c50b492a20
casperWein <19201040.casperweinberger@gmail.com>
2023-11-05 01:02:25 +0700

    v2.2.0

--- controller/MatKulController.js
+++ controller/MatKulController.js
@@ -1,1 +1,13 @@
-const NilaiMhs = require("../models/index").nilai_mahasiswa
+const MataKuliah = require("../models/index").dmatkul
+const {resError, response, invalidRequestRespon}  = require("./../utils/api-payload-response")
+
+const createDosen = async(req, res) => {
+    const dataDosen = {kodematkul, matkul, sks, smt}= req.body
+
+    await MataKuliah.create(dataDosen).then(d => {
+        response(201, "Success", d, res)
+    }).catch(error => {
+        console.log(error)
+        resError(500, process.env.ISE, error, res)
+    })
+}
